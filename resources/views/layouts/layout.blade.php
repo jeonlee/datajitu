6703d50e4fcbd086d297d8520e4b7bcbb45db3c9
jeonlee <elvanjeonlee89@gmail.com>
2016-08-26 14:43:41 +0700

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -81,9 +81,7 @@
                 </ol>
               </div>
           </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-          </ul>
+          
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             @yield('content')
