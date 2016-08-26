14f1b9887ba2067bbf743a1de986ff4d890e6a3e
jeonlee <elvanjeonlee89@gmail.com>
2016-08-26 13:42:27 +0700

    Update show.blade.php

--- resources/views/chinas/show.blade.php
+++ resources/views/chinas/show.blade.php
@@ -12,6 +12,9 @@
             <div>
                 Title : {{ $chinas->title }}
             </div>
+            <div class="text-center">
+                <a href="{{ url('/admin/china') }}" class="btn btn-primary">Back</a>
+            </div>
         </div>
     </div>
 @endsection
