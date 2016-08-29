228cb0789bc47a02058b7129c95cc66318f34454
jeonlee <elvanjeonlee89@gmail.com>
2016-08-29 13:59:03 +0700

    Add files via upload

---
+++ resources/views/index/carahitung.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.index')
+
+@section('content')
+
+<div class="panel panel-primary">
+  <div class="panel-heading p15 black font">
+    <div class="panel-title">
+      <h3>HASIL LENGKAP TOGEL TAIWAN</h3>
+    </div>
+  </div>
+
+  <div class="panel-body p15">
+    <p>
+      Contoh nomor bola yang dihasilkan singapore toto (www.singaporepools.com.sg): 
+      <br>
+      <br>
+      <div class="text-center" style="margin-left:150px;"><img src="images/tutorial.jpg"></div>
+      <br>
+      <p style="text-align:left;">Hasil Keluar Togel Singapura :</p>
+      <br>
+      <div class="text-center">
+      <table class="table table-bordered" style="width:50%;">
+        <tr>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Ribuan</td>
+          <td>Ratusan</td>
+          <td>Puluhan</td>
+          <td>Satuan</td>
+        </tr>
+      </table>
+      </div>
+      <br>
+        Cara manual perhitungan Hasil Toto Singapura (senin & kamis) menggunakan <b>Rumus Konversi Singapura terbaru</b> :<br>
+        <ol>
+          <li>Urutkan bola mulai dari yang terkecil sampai yang terbesar<br>
+            05 – 12 – 17 – 25 – 46 – 49</li>
+          <li>Cara mendapatkan angka Satuan / Ekor,<br>
+            (rumus = bola1 + bola2 + bola3 + bola4 + bola5 + bola6)<br>
+            05 + 12 + 17 + 25 + 46 + 49 = 15<span style="color:#F00;">4</span></li>
+          <li>Cara mendapatkan angka Puluhan / Kepala, <br>
+            (rumus = bola1 + bola2 + bola3 + bola4 + bola5 + bola6 + Additional Number)<br>
+            05 + 12 + 17 + 25 + 46 + 49 + 23 = 17<span style="color:#F00;">7</span></li>
+          <li>Cara mendapatkan angka Ratusan / KOP, <br>
+           (rumus = bola4 + bola5 + bola6)<br>
+            25 + 46 + 49 = 12<span style="color:#F00;">0</span></li>
+          <li>Cara mendapatkan angka Ribuan / AS,<br>
+        (rumus = bola1 + bola2 + bola3)<br>
+            05 + 12 + 17 = 3<span style="color:#F00;">4</span></li>
+        </ol>
+        <br>
+        maka hasil perhitungan diatas <br>
+        Di ambil angka belakangnya saja<br>
+        hasilnya = 
+        <table class="table table-bordered text-center" style="width:50%;">
+          <tr>
+            <td>4</td>
+            <td>0</td>
+            <td>7</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>Ribuan</td>
+            <td>Ratusan</td>
+            <td>Puluhan</td>
+            <td>Satuan</td>
+          </tr>
+        </table>
+
+        Untuk melihat hasil dari putaran nomor toto singapura ini bisa dilihat di <a href="index.php?hasiltoto">halaman ini.</a>
+
+      </p>
+    </div>
+  </div>
+
+@endsection
+
+@section('numbergenerator')
+
+
+              <div class="panel panel-default" style="margin-top:15px;">
+                <div class="panel-heading pink">
+                  <h3 class="panel-title white">NUMBER GENERATOR</h3>
+                </div>
+                <div class="panel-body p15">
+                  <p class="helvetica" style="text-align:center;">Generate your numbers</p>
+                  <br>
+                  <a title="Lottery Results - Lotto" class="button-blue"><button style="border:none; background:transparent;">Generate Numbers</button></a><br><br>
+                  <div class="group">
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                  </div>
+                </div>
+              </div>
+@endsection
---
+++ resources/views/index/konversi.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.index')
+
+@section('content')
+
+<div class="panel panel-primary">
+  <div class="panel-heading p15 black font">
+    <div class="panel-title">
+      <h3>HASIL LENGKAP TOGEL TAIWAN</h3>
+    </div>
+  </div>
+
+  <div class="panel-body">
+    <form name="fom">
+      <table class="table table-bordered text-center" style="width:55%; margin:auto;">
+        <thead>
+          <tr class="active">
+            <th colspan="6" class="text-center pink">Winning Numbers</th>
+            <th class="text-center pink">Additional Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s1" name="s1"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s2" name="s2"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s3" name="s3"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s4" name="s4"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s5" name="s5"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s6" name="s6"></td>
+            <td><input type="text" onkeypress="huruf(this,event);" class="form-control input-sm isi" maxlength="2" tabindex="1" id="s7" name="s7"></td>
+          </tr>
+          <tr>
+            <td colspan="3"><input type="button" class="btn btn-danger btn-xs" value="Hapus" onclick="hapusx()" tabindex="8" id="hapus" name="hapus"> <input type="button" class="btn btn-primary btn-xs" onclick="convert()" value="Convert!" tabindex="8" id="conv" name="conv">
+            <td colspan="4">
+            <div style="display:none" name="hsl" id="hsl">
+              <table cellspacing="9" cellpadding="0" bordercolor="silver" border="0" align="center">
+                <input type="hidden" name="wn1" id="wn1" class="td2"><input type="hidden" name="wn2" id="wn2" class="td2"><input type="hidden" name="wn3" id="wn3" class="td2"><input type="hidden" name="wn4" id="wn4" class="td2"><input type="hidden" name="wn5" id="wn5" class="td2"><input type="hidden" name="wn6" id="wn6" class="td2">
+                <input type="hidden" name="wn7" id="wn7" style="border:solid silver .5pt;border-color:silver;" colspan="2">
+                <tbody>
+                  <tr>
+                    <td colspan="6">
+                      <table cellspacing="0" cellpadding="0" border="0" align="center">
+                        <tbody>
+                          <tr height="15"><td style="font-size:13pt" colspan="4"><b>Hasil Konversi</b></td></tr>
+                          <tr><td height="9" colspan="4">&nbsp;</td></tr>
+                          <tr style="font-size=18px;font-weight:bolder;"><td name="ab1" id="ab1" class="td1">1</td><td name="ab2" id="ab2" class="td1">2</td><td name="ab3" id="ab3" class="td1">3</td><td name="ab4" id="ab4" class="td1">4</td></tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
+
+                
+                </div>
+              </div>
+
+@endsection
+
+@section('numbergenerator')
+
+
+              <div class="panel panel-default" style="margin-top:15px;">
+                <div class="panel-heading pink">
+                  <h3 class="panel-title white">NUMBER GENERATOR</h3>
+                </div>
+                <div class="panel-body p15">
+                  <p class="helvetica" style="text-align:center;">Generate your numbers</p>
+                  <br>
+                  <a title="Lottery Results - Lotto" class="button-blue"><button style="border:none; background:transparent;">Generate Numbers</button></a><br><br>
+                  <div class="group">
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                    <div class="reel"></div>
+                  </div>
+                </div>
+              </div>
+@endsection
