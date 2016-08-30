1bf1572c10d1caea9688d554677304fa89a3d2ab
jeonlee <elvanjeonlee89@gmail.com>
2016-08-30 17:00:08 +0700

    Update create.blade.php

--- resources/views/chinas/create.blade.php
+++ resources/views/chinas/create.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="form-group">
                     <label for="title">Tanggal</label>
-                    <input type="text" name="tanggal" id="tanggal" class="form-control input-sm" placeholder="dd-mm-yyyy">
+                    <input type="text" name="tanggal" id="datepicker" class="form-control input-sm" placeholder="dd-mm-yyyy">
                 </div>
                 <div class="form-group">
                     <label for="title">Periode</label>
