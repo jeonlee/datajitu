e4346f52174828f9b710af54527ecf4e562db0df
jeonlee <elvanjeonlee89@gmail.com>
2016-09-01 16:19:10 +0700

    Update HongkongController.php

--- app/Http/Controllers/HongkongController.php
+++ app/Http/Controllers/HongkongController.php
@@ -10,7 +10,7 @@ class HongkongController extends Controller
 {
     public function index()
 	{
-    	$hongkongs = Hongkong::orderBy('tanggal','desc')->paginate(5);
+    	$hongkongs = Hongkong::orderBy('periode','desc')->paginate(5);
 
     	return view('hongkongs.index', compact('hongkongs'));
 	}
@@ -23,7 +23,7 @@ class HongkongController extends Controller
 	{
         Hongkong::create($request->all());
 
-        $hongkongs = Hongkong::orderBy('tanggal','desc')->paginate(5);
+        $hongkongs = Hongkong::orderBy('periode','desc')->paginate(5);
 
         return view('hongkongs.index', compact('hongkongs'));
 	}
@@ -50,7 +50,7 @@ class HongkongController extends Controller
 
         $hongkong->fill($input)->save();
 
-        $hongkongs = Hongkong::orderBy('tanggal','desc')->paginate(5);
+        $hongkongs = Hongkong::orderBy('periode','desc')->paginate(5);
 
         return view('hongkongs.index', compact('hongkongs'));
     }
