729227369a773998304a8bb28d5de59fcf9ee4d2
jeonlee <elvanjeonlee89@gmail.com>
2016-09-07 15:44:49 +0700

    Update singapura.blade.php

--- resources/views/index/singapura.blade.php
+++ resources/views/index/singapura.blade.php
@@ -30,7 +30,7 @@
                   </div>
                   <div class="col-xs-7">
                     
-                    <h3 style="color: #FFF;"><span class="smallerHeading">Hasil Keluaran </span><br>{{ $singapura->hari }} <br><span class="smallerHeading">{{ $singapura->tanggal }}</span></h3>
+                    <h3 style="color: #FFF;"><span class="smallerHeading">Hasil Keluaran </span><br>{{ $singapura->hari }} <br><span class="smallerHeading">{{ Carbon\Carbon::parse($singapura->tanggal)->format('d-m-Y') }}</span></h3>
                   </div>
                 </div><!-- sideheader -->
                 <div style=" padding: 12px 2px 0 0;" class="floatRight centred">
