52588c10799cb432c7eb83ce5d1a2cd083333f3a
jeonlee <elvanjeonlee89@gmail.com>
2016-09-09 15:08:12 +0700

    Update analisishongkong.blade.php

--- resources/views/index/analisishongkong.blade.php
+++ resources/views/index/analisishongkong.blade.php
@@ -1143,14 +1143,14 @@
                     $dua = substr("$hongkong->number",1,1);
                     $tiga = substr("$hongkong->number",2,1);
                     $empat = substr("$hongkong->number",3,1);
-                    $tigash = substr("$hongkong->number",1,1) % 2;
+                    $tigash = substr("$hongkong->number",2,1) % 2;
                     $sh = 0;
                         if ($tigash == $sh) {
                             $tigash1 = "genap";
                         } else {
                             $tigash1 = "ganjil";
                         }
-                    $empatsh = substr("$hongkong->number",2,1) % 2;
+                    $empatsh = substr("$hongkong->number",3,1) % 2;
                     $hs = 0;
                         if ($empatsh == $hs) {
                             $empatsh1 = "genap";
