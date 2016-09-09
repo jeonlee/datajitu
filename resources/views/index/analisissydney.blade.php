faa7dca9c82acb5d2b1941ac484596285813a86b
jeonlee <elvanjeonlee89@gmail.com>
2016-09-09 15:08:58 +0700

    Update analisissydney.blade.php

--- resources/views/index/analisissydney.blade.php
+++ resources/views/index/analisissydney.blade.php
@@ -1143,14 +1143,14 @@
                     $dua = substr("$sydney->number",1,1);
                     $tiga = substr("$sydney->number",2,1);
                     $empat = substr("$sydney->number",3,1);
-                    $tigash = substr("$sydney->number",1,1) % 2;
+                    $tigash = substr("$sydney->number",2,1) % 2;
                     $sh = 0;
                         if ($tigash == $sh) {
                             $tigash1 = "genap";
                         } else {
                             $tigash1 = "ganjil";
                         }
-                    $empatsh = substr("$sydney->number",2,1) % 2;
+                    $empatsh = substr("$sydney->number",3,1) % 2;
                     $hs = 0;
                         if ($empatsh == $hs) {
                             $empatsh1 = "genap";
