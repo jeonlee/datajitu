85a1e5e58fa137c7290acd92f7552a5da21d6195
jeonlee <elvanjeonlee89@gmail.com>
2016-09-09 15:15:01 +0700

    Update analisiscambodia.blade.php

--- resources/views/index/analisiscambodia.blade.php
+++ resources/views/index/analisiscambodia.blade.php
@@ -52,7 +52,7 @@
             <li><a data-toggle="tab" href="#colokjituas">As</a></li>
             <li><a data-toggle="tab" href="#colokjitukop">Kop</a></li>
             <li><a data-toggle="tab" href="#colokjitukepala">Kepala</a></li>
-            <li class="divider"></li>
+            
             <li><a data-toggle="tab" href="#colokjituekor">Ekor</a></li>
             
           </ul>
@@ -65,7 +65,7 @@
             <li><a data-toggle="tab" href="#ganjilgenapas">As</a></li>
             <li><a data-toggle="tab" href="#ganjilgenapkop">Kop</a></li>
             <li><a data-toggle="tab" href="#ganjilgenapkepala">Kepala</a></li>
-            <li class="divider"></li>
+            
             <li><a data-toggle="tab" href="#ganjilgenapekor">Ekor</a></li>
             
           </ul>
