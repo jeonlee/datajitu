608788e48eecf5303e1703183192b83011dc2c1f
jeonlee <elvanjeonlee89@gmail.com>
2016-10-31 13:34:38 +0700

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -256,8 +256,9 @@
     <script type="text/javascript" src="js/slot.js"></script>
     <script type="text/javascript" src="js/slot1.js"></script>
     <script type="text/javascript" src="js/search.js"></script>
-     <script type="text/javascript" src="js/jquery.lettering.js"></script>
-     <script src="/js/jquery-ui.js"></script>
+    <script src="//rawgithub.com/stidges/jquery-searchable/master/dist/jquery.searchable-1.0.0.min.js"></script>
+    <script type="text/javascript" src="js/jquery.lettering.js"></script>
+    <script src="/js/jquery-ui.js"></script>
     <script>
           $( function() {
     var dateFormat = "yy-mm-dd",
