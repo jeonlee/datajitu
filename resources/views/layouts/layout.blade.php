96a888a3f6bf5ea5016563150a008d718b83394d
jeonlee <elvanjeonlee89@gmail.com>
2016-11-10 13:29:49 +0700

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -76,7 +76,18 @@
                   <li><a href="{{ url('/admin/china') }}">Togel China</a></li>
                   <li><a href="{{ url('/admin/hongkong') }}">Togel Hongkong</a></li>
 
-               
+               <li><a href="#" data-toggle="collapse" data-target="#prediksis" aria-expanded="false" aria-controls="results">Prediksi Togel <span class="pull-right"><i class="fa fa-caret-down" aria-hidden="true"></i></span></a></li>
+              <div class="collapse" id="prediksis">
+                <ol>
+                  <li><a href="{{ url('/admin/prediksisgp') }}">Togel Singapore</a></li>
+                  <li><a href="{{ url('/admin/prediksisyd') }}">Togel Sydney</a></li>
+                  <li><a href="{{ url('/admin/prediksicmb') }}">Togel Cambodia</a></li>
+                  <li><a href="{{ url('/admin/prediksitw') }}">Togel Taiwan</a></li>
+                  <li><a href="{{ url('/admin/prediksichn') }}">Togel China</a></li>
+                  <li><a href="{{ url('/admin/prediksihk') }}">Togel Hongkong</a></li>
+
+                </ol>
+              </div>
           </ul>
           
         </div>
