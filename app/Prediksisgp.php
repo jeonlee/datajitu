af199bc43cf112ecb79ce6b60dc3ad41f9870cf7
jeonlee <elvanjeonlee89@gmail.com>
2016-11-10 13:15:24 +0700

    Add files via upload

---
+++ app/Prediksisgp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prediksisgp extends Model
+{
+   protected $fillable = [
+        'tanggal', 'hari', 'prediksi', 'date'
+    ];
+    public function path()
+    {
+    	return '/categories/' . $this->id;
+    }
+
+    public function reports()
+    {
+    	return $this->hasMany('App\Report');
+    }
+
+    public function addReport(Report $report)
+    {
+    	return $this->reports()->save($report);
+    }
+}
