f558580130e70fcf8ba94ca00d67e818416bbd71
jeonlee <elvanjeonlee89@gmail.com>
2016-12-06 16:42:34 +0700

    Update index.blade.php

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -73,7 +73,7 @@
                  ?>
             <div class="col-xs-12" style="margin-top:15px;">
               <h3 class="josefin">TOGEL CAMBODIA</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
                 <div class="sideHeader lotto">
                   <div class="col-xs-5">
                     <img width="100%" src="images/cambodia.png">
@@ -88,11 +88,27 @@
                 <div class="result medium lotto-ball"><?php  echo $dua; ?></div>
                 <div class="result medium lotto-ball"><?php  echo $tiga; ?></div>
                 <div class="result medium lotto-ball"><?php  echo $empat; ?></div>
-           
+
                 <br><br>
                 <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasilcambodia') }}">Hasil Lengkap</a>
               </div>
             </div>
+            
+            <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader lotto" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $cambodia->hari }}</span> <br><span class="smallerHeading">{{ $cambodia->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium lotto-ball"><?php  echo $satu; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $dua; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $tiga; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasilcambodia') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+              </div><!-- end resultbox -->
+              
           </div><!-- end col-xs-6 -->
           @endforeach
 
@@ -106,7 +122,7 @@
                  ?>
           <div class="col-xs-12" style="margin-top:15px;">
           <h3 class="josefin">TOGEL SYDNEY</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
             <div class="sideHeader hotpicks">
             <div class="col-xs-5">
                 <img width="100%" src="images/sydney.png">
@@ -128,6 +144,22 @@
           <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasilsydney') }}">Hasil Lengkap</a>
             </div>
           </div>
+          
+          <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader hotpicks" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $sydney->hari }}</span> <br><span class="smallerHeading">{{ $sydney->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium lotto-ball"><?php  echo $satu; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $dua; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $tiga; ?></div>
+                  <div class="result medium lotto-ball"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasilsydney') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+              </div><!-- end resultbox -->
+              
           </div><!-- end col-xs-6 -->
           @endforeach
 
@@ -141,7 +173,7 @@
                  ?>
           <div class="col-xs-12" style="margin-top:15px;">
           <h3 class="josefin">TOGEL CHINA</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
             <div class="sideHeader health">
              <div class="col-xs-5">
                 <img width="100%" src="images/china.png">
@@ -163,6 +195,23 @@
           <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasilchina') }}">Hasil Lengkap</a>
             </div>
           </div>
+          
+          <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader health" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $china->hari }}</span> <br><span class="smallerHeading">{{ $china->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium health-ball"><?php  echo $satu; ?></div>
+                  <div class="result medium health-ball"><?php  echo $dua; ?></div>
+                  <div class="result medium health-ball"><?php  echo $tiga; ?></div>
+                  <div class="result medium health-ball"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasilchina') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+              </div><!-- end resultbox -->
+              
+              
           </div><!-- end col-xs-6 -->
           @endforeach
 
@@ -177,7 +226,7 @@
                  ?>
           <div class="col-xs-12" style="margin-top:15px;">
           <h3 class="josefin">TOGEL TAIWAN</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
             <div class="sideHeader postcode">
              <div class="col-xs-5">
                 <img width="100%" src="images/taiwan.png">
@@ -199,6 +248,23 @@
           <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasiltaiwan') }}">Hasil Lengkap</a>
             </div>
           </div>
+          
+          <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader postcode" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $taiwan->hari }}</span> <br><span class="smallerHeading">{{ $taiwan->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium euromillions-lucky-star"><?php  echo $satu; ?></div>
+                  <div class="result medium euromillions-lucky-star"><?php  echo $dua; ?></div>
+                  <div class="result medium euromillions-lucky-star"><?php  echo $tiga; ?></div>
+                  <div class="result medium euromillions-lucky-star"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasiltaiwan') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+              </div><!-- end resultbox -->
+              
+              
           </div><!-- end col-xs-6 -->
           @endforeach
 
@@ -212,7 +278,7 @@
                  ?>
           <div class="col-xs-12" style="margin-top:15px;">
           <h3 class="josefin">TOGEL HONGKONG</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
             <div class="sideHeader thunderball">
              <div class="col-xs-5">
                 <img width="100%" src="images/hk.png">
@@ -234,7 +300,22 @@
           <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasilhongkong') }}">Hasil Lengkap</a>
             </div>
           </div>
-
+            
+            <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader thunderball" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $hongkong->hari }}</span> <br><span class="smallerHeading">{{ $hongkong->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium thunderball-ball"><?php  echo $satu; ?></div>
+                  <div class="result medium thunderball-ball"><?php  echo $dua; ?></div>
+                  <div class="result medium thunderball-ball"><?php  echo $tiga; ?></div>
+                  <div class="result medium thunderball-ball"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasilhongkong') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+              </div><!-- end resultbox -->
+              
           </div><!-- end col-xs-6 -->
           @endforeach
 
