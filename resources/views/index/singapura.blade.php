9b6b81b5269f5071a8b9dc9edc49536985926619
jeonlee <elvanjeonlee89@gmail.com>
2016-12-08 14:28:22 +0700

    Update singapura.blade.php

--- resources/views/index/singapura.blade.php
+++ resources/views/index/singapura.blade.php
@@ -23,7 +23,7 @@
                  ?>
                   <div class="col-xs-12" style="margin-top:15px;">
               <h3 class="josefin">TOGEL SINGAPURA</h3>
-              <div class="resultBox shorter centred">
+              <div class="resultBox shorter centred hidden-sm hidden-xs">
                 <div class="sideHeader euromillions">
                   <div class="col-xs-5">
                     <img width="100%" src="images/singapura.png">
@@ -45,6 +45,23 @@
                   <a class="button-blue" title="Lottery Results - Lotto" href="{{ url('/hasilsingapura') }}">Hasil Lengkap</a>
                 </div>
               </div>
+              
+              <div class="resultBox shorter centred visible-xs visible-sm" style="width: 100%;">
+                <div class="sideHeader euromillions" style="height: 150px; width: 100%; padding-top:0px; padding-bottom: 0px;">
+                  <div class="col-xs-12">
+                    
+                    <h3 style="color: #FFF;"><span class="smallerHeading">{{ $singapura->hari }}</span> <br><span class="smallerHeading">{{ $singapura->tanggal }}</span></h3>
+                  </div>
+                  <div class="result medium euromillions-ball"><?php  echo $satu; ?></div>
+                  <div class="result medium euromillions-ball"><?php  echo $dua; ?></div>
+                  <div class="result medium euromillions-ball"><?php  echo $tiga; ?></div>
+                  <div class="result medium euromillions-ball"><?php  echo $empat; ?></div>
+                  <a class="button-blue-mobile" title="Lottery Results - Lotto" href="{{ url('/hasilsingapura') }}">Hasil Lengkap</a>
+                </div><!-- sideheader -->
+                <br><br>
+             </div><!-- end resultbox -->
+
+
             </div><!-- end col-xs-12 -->
 
              @endforeach
