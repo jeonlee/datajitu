faf91d05212f25c2b1185164cfbff59d1a49f756
jeonlee <elvanjeonlee89@gmail.com>
2016-12-08 11:14:21 +0700

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -95,7 +95,7 @@
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div class="collapse navbar-collapse nav-side-menu" id="bs-example-navbar-collapse-1">
                       <ul class="nav navbar-nav">
                         <li><a href="{{ url('/') }}">HOME</a></li>
                         <li><a href="{{ url('/singapura') }}">TOGEL SINGAPURA</a></li>
