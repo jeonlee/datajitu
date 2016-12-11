c2b55937b4bb4f3adcc15d8e1effe393fdc78dcd
jeonlee <elvanjeonlee89@gmail.com>
2016-12-11 11:36:02 +0700

    Update comingsoon.blade.php

--- resources/views/index/comingsoon.blade.php
+++ resources/views/index/comingsoon.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 	<div class="col-xs-12">
-	  <div class="panel-primary">
+	  <div class="panel panel-primary">
 	    <div class="panel-heading p15 black font">
 	      <div class="panel-title">
 	        PREDIKSI TOGEL
 	      </div>
 	    </div>  
-	    <div class="panel-body">
-	        <div style="text-align:center"><h2 class="shine">COMING SOON</h2></div>
+	    <div class="panel-body" style="height:500px;">
+	        <div style="text-align:center"><h2 class="shine" style="font-size:12px;">COMING SOON</h2></div>
 	        </div>
 	    </div>
 	  </div>
