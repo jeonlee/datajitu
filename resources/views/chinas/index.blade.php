67bfe981d22eaf443c7874655818751b0f7040da
jeonlee <elvanjeonlee89@gmail.com>
2017-07-20 15:47:50 +0700

    Update index.blade.php

--- resources/views/chinas/index.blade.php
+++ resources/views/chinas/index.blade.php
@@ -38,7 +38,7 @@
                                 <form method="POST" action="/admin/china/{{ $china->id }}/delete">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
-                                    <!-- <button type="submit" class="btn btn-danger btn-sm">Delete</button> -->             
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>             
                                 </form>
                             </td>
                         </tr>
