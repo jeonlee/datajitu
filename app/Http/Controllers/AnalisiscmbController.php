88a156504a468b8d48de6d258918e1cd36c713ae
jeonlee <elvanjeonlee89@gmail.com>
2017-09-23 10:43:45 +0700

    Update AnalisiscmbController.php

--- app/Http/Controllers/AnalisiscmbController.php
+++ app/Http/Controllers/AnalisiscmbController.php
@@ -46,7 +46,9 @@ class AnalisiscmbController extends Controller
         $ekorgg = $empat % 2;
         $dg = 0;
         $tigash = substr("$cambodia->number",2,1) % 2;
+	$empatsh = substr("$china->number",3,1) % 2;
         $sh = 0;
+	$hs = 0;
         $tt = substr("$cambodia->number",2,2);
         $dasarbk = $tiga + $empat;
         $dasarbk3 = substr("$dasarbk",0,1);
