c4aca1dc886c71e3fd1bc5ed18c162cc756874b8
jeonlee <elvanjeonlee89@gmail.com>
2017-12-04 19:27:26 +0700

    Update prediksisgp.blade.php

--- resources/views/index/prediksisgp.blade.php
+++ resources/views/index/prediksisgp.blade.php
@@ -2,40 +2,38 @@
 
 @section('content')
 
-<div class="col-xs-12">
-              <div class="panel panel-primary">
-                <div class="panel-heading p15 black font">
-                  <div class="panel-title">
-                    <h3>JADWAL KELUARAN TOGEL</h3>
-                  </div>
-                </div>
-                <div class="panel-body">
-                  <table class="middle">
-            <thead>
-              <tr class="kolom">
-                <th><div>No</div></th>
-                <th><div>Togel</div></th>
-                <th><div>Hari</div></th>                                    
-                <th><div class="th_jam">Tanggal</div></th>
-                <th><div class="th_jam">Prediksi</div></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><div class="t_no">1</div></td>
-                <td><div class="t_hari">Togel Singapura</div></td>
-                <td><div class="t_tgl">KAMIS</div></td>                                    
-                <td><div class="t_city">18-08-2018</div></td>
-                <td><div class="t_city">1234</div></td>
-              </tr>   
-              
-                                                                                                                                                                                                                                           
-            </tbody>
-        
-        
+  <div class="col-xs-12">
+    <div class="panel panel-primary">
+      <div class="panel-heading p15 black font">
+        <div class="panel-title">
+          <h3>JADWAL KELUARAN TOGEL</h3>
+        </div>
+      </div>
+      <div class="panel-body">
+        <table class="middle">
+          <thead>
+            <tr class="kolom">
+              <th><div>No</div></th>
+              <th><div>Togel</div></th>
+              <th><div>Hari</div></th>                                    
+              <th><div class="th_jam">Tanggal</div></th>
+              <th><div class="th_jam">Prediksi</div></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><div class="t_no">1</div></td>
+              <td><div class="t_hari">Togel Singapura</div></td>
+              <td><div class="t_tgl">{{ $prediksisgp->hari }}</div></td>                                    
+              <td><div class="t_city">{{ Carbon\Carbon::parse($prediksisgp->tanggal)->format('d-m-Y') }}</div></td>
+              <td><div class="t_city">{{ $prediksisgp->prediksi }}</div></td>
+            </tr>   
+           
+                                                                                                                                                                                                                                         
+          </tbody>
         </table>
-                </div>
-              </div>
-            </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
