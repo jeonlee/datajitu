290347eed722be47965ae3313981a475c70be83f
jeonlee <elvanjeonlee89@gmail.com>
2017-12-05 11:09:44 +0700

    Update prediksisgp.blade.php

--- resources/views/index/prediksisgp.blade.php
+++ resources/views/index/prediksisgp.blade.php
@@ -262,7 +262,7 @@
         
         </div>
         <div class="col-xs-7">
-          <h3 style="color: #FFF;"><span class="smallerHeading">Hasil Keluaran </span><br>{{ $prediksihk->hari }} <br><span class="smallerHeading">{{ Carbon\Carbon::parse($hongkong->tanggal)->format('d-m-Y') }}</span></h3>
+          <h3 style="color: #FFF;"><span class="smallerHeading">Hasil Keluaran </span><br>{{ $prediksihk->hari }} <br><span class="smallerHeading">{{ Carbon\Carbon::parse($prediksihk->tanggal)->format('d-m-Y') }}</span></h3>
         </div>
       </div><!-- sideheader -->
       <div style=" padding: 12px 2px 0 0;" class="floatRight centred">
