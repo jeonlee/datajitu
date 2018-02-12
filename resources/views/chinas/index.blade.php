d29b9ad93b0a4f23bfea490ae5186fce6a59f0d9
jeonlee <elvanjeonlee89@gmail.com>
2018-02-12 09:17:23 +0700

    Update index.blade.php

--- resources/views/chinas/index.blade.php
+++ resources/views/chinas/index.blade.php
@@ -34,13 +34,13 @@
                             <td>{{ $china->periode }}-CN</td>
                             <td>{{ $china->number }}</td>
                             <td><a href="/admin/china/{{ $china->id }}/edit" class="btn btn-default btn-sm">Edit</a></td>
-                            <td>
+                            <!-- <td>
                                 <form method="POST" action="/admin/china/{{ $china->id }}/delete">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
                                      <button type="submit" class="btn btn-danger btn-sm">Delete</button>            
                                 </form>
-                            </td>
+                            </td> -->
                         </tr>
                     @endforeach
                 </tbody>
