78df3c9de4f0f55c37b26a338d27cdad660cf447
jeonlee <elvanjeonlee89@gmail.com>
2018-05-05 17:01:04 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -68,9 +68,9 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <!-- <button type="submit" class="btn btn-primary">
+                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
-                                </button>-->
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -78,5 +78,5 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 @endsection
