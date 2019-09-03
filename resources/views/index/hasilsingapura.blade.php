0761532ec3ff9d5def1ae5fc4c0bd5e475d48042
jeonlee <elvanjeonlee89@gmail.com>
2019-09-03 15:28:46 +0700

    Update hasilsingapura.blade.php

--- resources/views/index/hasilsingapura.blade.php
+++ resources/views/index/hasilsingapura.blade.php
@@ -45,9 +45,10 @@
                 
             <form>
             <select id="pagination">
-                        <option value="5" @if($items == 5) selected @endif >5</option>
                         <option value="10" @if($items == 10) selected @endif >10</option>
                         <option value="25" @if($items == 25) selected @endif >25</option>
+                        <option value="50" @if($items == 50) selected @endif >50</option>
+                        <option value="100" @if($items == 100) selected @endif >100</option>
                     </select>
                 </form>
 
