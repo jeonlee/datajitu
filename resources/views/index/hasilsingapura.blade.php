00b08045573873027f7c2e647b1498b6b59e41b5
jeonlee <elvanjeonlee89@gmail.com>
2019-10-18 14:11:02 +0630

    Update hasilsingapura.blade.php

--- resources/views/index/hasilsingapura.blade.php
+++ resources/views/index/hasilsingapura.blade.php
@@ -47,18 +47,14 @@
         
             
                 <div class="text-center">
-                <div data-role="page"> 
-    <div data-role="content">
                 <form action="#" method="get">
-            <select id="pagination" data-native-menu="false">
+                   <select id="pagination" data-native-menu="false">
                         <option value="10" @if($items == 10) selected @endif >10</option>
                         <option value="25" @if($items == 25) selected @endif >25</option>
                         <option value="50" @if($items == 50) selected @endif >50</option>
                         <option value="100" @if($items == 100) selected @endif >100</option>
                     </select>
                 </form>
-            </div>
-            </div>
                
                  <script type="text/javascript">
                     document.getElementById('pagination').onchange = function() {
