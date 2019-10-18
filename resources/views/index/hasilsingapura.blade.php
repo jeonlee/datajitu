55d3d430ff0e499873596ff51193de10c8663382
jeonlee <elvanjeonlee89@gmail.com>
2019-10-18 10:31:47 +0630

    Update hasilsingapura.blade.php

--- resources/views/index/hasilsingapura.blade.php
+++ resources/views/index/hasilsingapura.blade.php
@@ -61,7 +61,7 @@
             </div>
                
                  <script>
-                    document.querySelectorAll('#pagination').onchange = function() {
+                    document.getElementById('pagination').onchange = function() {
                         window.location = "{{ $singapuras->url(1) }}&items=" + this.value;
                     };
                 </script>
