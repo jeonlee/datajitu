bfa1048848fb99606f37f7cd87878a922c84bb90
jeonlee <elvanjeonlee89@gmail.com>
2019-10-18 09:22:16 +0630

    Update hasilsingapura.blade.php

--- resources/views/index/hasilsingapura.blade.php
+++ resources/views/index/hasilsingapura.blade.php
@@ -61,7 +61,7 @@
                         window.location = "{{ $singapuras->url(1) }}&items=" + this.value;
                     };
                 </script>
-                {!! $singapuras->render(); !!}
+                {!! $singapuras->appends(['items' => '$items'])->render(); !!}
             </div>
               </div>
             </div><!-- col-xs-9 -->
