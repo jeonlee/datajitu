e10b456d43d7f0ad4a1be83410942788eb1b7e1e
jeonlee <elvanjeonlee89@gmail.com>
2019-11-04 14:59:06 +0630

    Update analisissingapura.blade.php

--- resources/views/index/analisissingapura.blade.php
+++ resources/views/index/analisissingapura.blade.php
@@ -343,7 +343,7 @@
                      if ($satu == 9)  $cjas9 = "&#x2714;";   else  $cjas9 = ""; 
                  ?>
                         <tr>
-                            <td style="display:none;"><?php  echo $total_satu; ?></td>
+                            <td style="display:none;"><?php  echo $as_sgp; ?></td>
                             <td>{{ $singapura->hari }}</td>
                             <td>{{ Carbon\Carbon::parse($singapura->tanggal)->format('d-m-Y') }}</td>
                             <td>{{ $singapura->periode }}-SG</td>
