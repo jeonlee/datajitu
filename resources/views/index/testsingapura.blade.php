da8558db954f99ae064152cd2d17da74a0159945
jeonlee <elvanjeonlee89@gmail.com>
2019-11-05 17:52:23 +0630

    Update testsingapura.blade.php

--- resources/views/index/testsingapura.blade.php
+++ resources/views/index/testsingapura.blade.php
@@ -19,7 +19,7 @@
                     $tiga = substr("$singapura->number",2,1);
                     $empat = substr("$singapura->number",3,1);
 		    $as=array_count_values($sgp_as);
-		    
+		    $as_0=$as['0'];
                     
                  ?>
                   <div class="col-xs-12" style="margin-top:15px;">
