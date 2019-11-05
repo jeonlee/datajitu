deb332868a04d1beb129f2d4986f81a70f739333
jeonlee <elvanjeonlee89@gmail.com>
2019-11-05 15:16:32 +0630

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -89,7 +89,7 @@ class IndexController extends Controller
         $sgp_kop=array();
         $sgp_kepala=array();
         $sgp_ekor=array();
-        foreach($singapuras as $sgp){
+        foreach($sgp_number as $sgp){
             $sgp_as[]=substr($sgp->number,0,1);  
             $sgp_kop[]=substr($sgp->number,1,1); 
             $sgp_kepala[]=substr($sgp->number,2,1); 
