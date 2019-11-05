f7cb2b90ce72787ce6e758d43b282ac257ebb8c7
jeonlee <elvanjeonlee89@gmail.com>
2019-11-05 17:06:25 +0630

    Update testsingapura.blade.php

--- resources/views/index/testsingapura.blade.php
+++ resources/views/index/testsingapura.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <div class="panel-body">
-                @foreach ($sorted->slice(0, 1) as $singapura)
+                @foreach ($sgp_number as $singapura)
                 <?php
 
                     $satu = substr("$singapura->number",0,1);
