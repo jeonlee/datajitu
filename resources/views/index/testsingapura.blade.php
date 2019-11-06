790f638792679251c492ecf9a3d6c0380cc1301c
jeonlee <elvanjeonlee89@gmail.com>
2019-11-06 15:37:58 +0630

    Update testsingapura.blade.php

--- resources/views/index/testsingapura.blade.php
+++ resources/views/index/testsingapura.blade.php
@@ -101,7 +101,7 @@
 			<div class="frequency-graph">
 
 				<div class="frequency-bar">
-					<div class="bar" id="bar-0-0" style="height: 30%;"> <div>
+					<div class="bar" id="bar-0-0" style="height: 30%;">{{ count($gg_as) }} <div>
 						{{ (!empty($as['0'])) ? $as['0'] : '0' }}</div>
 				</div>
 				<div class="number">0</div>
