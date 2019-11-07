64686c6d158263e06f61b8b56407419defc02f87
jeonlee <elvanjeonlee89@gmail.com>
2019-11-07 17:54:17 +0630

    Update testsingapura.blade.php

--- resources/views/index/testsingapura.blade.php
+++ resources/views/index/testsingapura.blade.php
@@ -450,6 +450,7 @@
 <script> // AS
 	var as = <?php echo $tes3; ?>
 	var str_as = as.toString();
+	console.log(str_as);
 	let drawAs = str_as.split(",");
 	for (let i = 0; i < 10; i++) {
 		setTimeout(() => {
