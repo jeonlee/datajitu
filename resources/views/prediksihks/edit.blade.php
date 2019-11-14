e3f86331d9322ae82d210edc57aa0fdd500f083a
jeonlee <elvanjeonlee89@gmail.com>
2019-11-14 14:02:51 +0630

    Update edit.blade.php

--- resources/views/prediksihks/edit.blade.php
+++ resources/views/prediksihks/edit.blade.php
@@ -4,21 +4,21 @@
     <div class="panel panel-default">
         <div class="panel-heading">
         	<div class="panel-title">
-        		Edit {{ $hongkongs->periode }}
+        		Edit {{ $prediksihks->periode }}
         	</div>
         </div>
 
         <div class="panel-body">
-        	<form method="POST" action="/admin/hongkong/{{ $hongkongs->id }}">
+        	<form method="POST" action="/admin/hongkong/{{ $prediksihks->id }}">
         		{{ csrf_field() }}
                 {{ method_field('PATCH') }}
                 <div class="form-group">
                     <label for="title">Periode</label>
-                    <input type="text" name="periode" id="periode" class="form-control input-sm" placeholder="{{ $hongkongs->periode }}">
+                    <input type="text" name="periode" id="periode" class="form-control input-sm" placeholder="{{ $prediksihks->periode }}">
                 </div>
                 <div class="form-group">
                     <label for="title">Number</label>
-                    <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $hongkongs->number }}">
+                    <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $prediksihks->number }}">
                 </div>
 				<button type="submit" class="btn btn-default">Submit</button>
 			</form>
