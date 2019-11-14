cedee6d04662fe7eea8c497f08e56132011b7ebd
jeonlee <elvanjeonlee89@gmail.com>
2019-11-14 13:29:11 +0630

    Update edit.blade.php

--- resources/views/prediksicmbs/edit.blade.php
+++ resources/views/prediksicmbs/edit.blade.php
@@ -4,21 +4,21 @@
     <div class="panel panel-default">
         <div class="panel-heading">
         	<div class="panel-title">
-        		Edit {{ $cambodias->periode }}
+        		Edit {{ $prediksicmbs->periode }}
         	</div>
         </div>
 
         <div class="panel-body">
-        	<form method="POST" action="/admin/cambodia/{{ $cambodias->id }}">
+        	<form method="POST" action="/admin/cambodia/{{ $prediksicmbs->id }}">
         		{{ csrf_field() }}
                 {{ method_field('PATCH') }}
                 <div class="form-group">
                     <label for="title">Periode</label>
-                    <input type="text" name="periode" id="periode" class="form-control input-sm" placeholder="{{ $cambodias->periode }}">
+                    <input type="text" name="periode" id="periode" class="form-control input-sm" placeholder="{{ $prediksicmbs->periode }}">
                 </div>
                 <div class="form-group">
                     <label for="title">Number</label>
-                    <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $cambodias->number }}">
+                    <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $prediksicmbs->number }}">
                 </div>
 				<button type="submit" class="btn btn-default">Submit</button>
 			</form>
