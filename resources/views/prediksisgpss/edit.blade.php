9cd5a8a49df35df671c8f6021488f87eb5204834
jeonlee <elvanjeonlee89@gmail.com>
2019-11-14 13:18:41 +0630

    Update edit.blade.php

--- resources/views/prediksisgpss/edit.blade.php
+++ resources/views/prediksisgpss/edit.blade.php
@@ -21,7 +21,7 @@
                     <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $prediksisgps->number }}">
                 </div>
                 <div class="form-group">
-                    <label for="title">Deskripsi</label>
+                    <label for="title">Deskripsi</label>{{ $prediksisgps->number }}
                     <textarea for="text" class="form-control">{{ $prediksisgps->deskripsi }}</textarea>
                 </div>
 				<button type="submit" class="btn btn-default">Submit</button>
