59c29d9138f46e127ddf78bda72f87448f279ced
jeonlee <elvanjeonlee89@gmail.com>
2019-11-18 11:29:10 +0630

    Create prediksichndet.blade.php

---
+++ resources/views/index/prediksichndet.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.index')
+
+@section('content')
+  <div class="panel panel-primary">
+    <div class="panel-heading p15 black font">
+      <div class="panel-title">
+        <h3>Prediksi Togel China</h3>
+      </div>
+    </div>
+
+    <div class="panel-body">
+      {!! $prediksichn->deskripsi !!}
+    </div>
+  </div>
+  
+ 
+@endsection
