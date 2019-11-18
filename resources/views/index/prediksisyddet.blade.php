2142a2afec498a0da3f07cb9ead3a977440f238b
jeonlee <elvanjeonlee89@gmail.com>
2019-11-18 11:28:46 +0630

    Create prediksisyddet.blade.php

---
+++ resources/views/index/prediksisyddet.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.index')
+
+@section('content')
+  <div class="panel panel-primary">
+    <div class="panel-heading p15 black font">
+      <div class="panel-title">
+        <h3>Prediksi Togel Sydney</h3>
+      </div>
+    </div>
+
+    <div class="panel-body">
+      {!! $prediksisyd->deskripsi !!}
+    </div>
+  </div>
+  
+ 
+@endsection
