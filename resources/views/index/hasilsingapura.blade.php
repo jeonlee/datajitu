142db68b84a1323e8a2b23e310fdc0636559b9d5
jeonlee <elvanjeonlee89@gmail.com>
2020-02-11 17:45:16 +0800

    Update hasilsingapura.blade.php

--- resources/views/index/hasilsingapura.blade.php
+++ resources/views/index/hasilsingapura.blade.php
@@ -63,7 +63,23 @@
                 </script>
                 {!! $singapuras->appends(['items' => $items])->render(); !!}
             </div>
-            
+             <!--<div class="text-center visible-sm visible-xs">
+                <form action="#">
+                   <select id="paginations" name="items">
+                        <option value="10" @if($items == 10) selected @endif >10</option>
+                        <option value="25" @if($items == 25) selected @endif >25</option>
+                        <option value="50" @if($items == 50) selected @endif >50</option>
+                        <option value="100" @if($items == 100) selected @endif >100</option>
+                    </select>
+                </form>
+               
+                 <script type="text/javascript">
+                    jquery(document).getElementById('paginations').onchange = function() {
+                        window.location.href = "{{ $singapuras->url(1) }}&items=" + this.value;
+                    };
+                </script>
+                {!! $singapuras->appends(['items' => $items])->render(); !!}
+            </div>-->
            
             
             
