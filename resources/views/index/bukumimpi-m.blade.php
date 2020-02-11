b7ef9b1a4ae7b2bb2d81a75d52c0b0edefbffe34
jeonlee <elvanjeonlee89@gmail.com>
2020-02-11 09:12:08 +0800

    Update bukumimpi-m.blade.php

--- resources/views/index/bukumimpi-m.blade.php
+++ resources/views/index/bukumimpi-m.blade.php
@@ -2,8 +2,33 @@
 
 @section('content')
 
+
+<style type="text/css">
+/*Form styles*/
+.styled fieldset, .styled fieldset ol, .styled fieldset li {border: none; padding: 0; margin: 0;}
+.styled {font-family: Arial, sans-serif; font-size: 15px; color: #333;}
+.styled fieldset {padding: 20px 0}
+.styled input.text-input {background: none; border: 1px solid #ccc; padding: 5px; width: 246px; font-size: 16px; line-height: 1em;}
+</style>
+
+<div class="row">
+        <div class="col-xs-12">
+            <form id="live-search" action="" class="styled" method="post">
+                <fieldset>
+                    <input type="text" class="text-input" id="filter" value="" />
+                    <span id="filter-count"></span>
+                </fieldset>
+            </form>
+        </div>
+        <ol class="commentlist">
+          <li>aaaaaaaaaaaaa</li>
+          <li>bbbbbbbbbbbbb</li>
+        </ol>
+  </div>
+    
+    
 <script type="text/javascript">
-  jQuery(document).ready(function(){
+  $(document).ready(function(){
     $("#filter").keyup(function(){
  
         // Retrieve the input field text and reset the count to zero
@@ -29,31 +54,6 @@
     });
 });
 </script>
-<style type="text/css">
-/*Form styles*/
-.styled fieldset, .styled fieldset ol, .styled fieldset li {border: none; padding: 0; margin: 0;}
-.styled {font-family: Arial, sans-serif; font-size: 15px; color: #333;}
-.styled fieldset {padding: 20px 0}
-.styled input.text-input {background: none; border: 1px solid #ccc; padding: 5px; width: 246px; font-size: 16px; line-height: 1em;}
-</style>
-
-<div class="row">
-        <div class="col-xs-12">
-            <form id="live-search" action="" class="styled" method="post">
-                <fieldset>
-                    <input type="text" class="text-input" id="filter" value="" />
-                    <span id="filter-count"></span>
-                </fieldset>
-            </form>
-        </div>
-        <ol class="commentlist">
-          <li>aaaaaaaaaaaaa</li>
-          <li>bbbbbbbbbbbbb</li>
-        </ol>
-  </div>
-    
-    
-
 
 
 
