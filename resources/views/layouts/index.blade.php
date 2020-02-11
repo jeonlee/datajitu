b13b9c2524f170db58f0f4b0c3c6205938c63041
jeonlee <elvanjeonlee89@gmail.com>
2020-02-11 09:56:48 +0800

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -806,7 +806,7 @@ $(function () {
   </script>
   <script type="text/javascript">
   $(document).ready(function(){
-    $("#filter").keyup touchend(function(){
+    $("#filter").keyup(function(){
  
         // Retrieve the input field text and reset the count to zero
         var filter = $(this).val(), count = 0;
