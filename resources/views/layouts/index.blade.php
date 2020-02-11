0c2abc8ef2f00089660a948b96b53d2ac0964c93
jeonlee <elvanjeonlee89@gmail.com>
2020-02-11 14:09:19 +0800

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -655,7 +655,7 @@
     </footer>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
     <!-- <script src="/js/jquery-1.7.1.min.js" type="text/javascript"></script> -->
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="/js/bootstrap.min.js"></script>
