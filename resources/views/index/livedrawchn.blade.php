c9ad82439aedfb8b847a6c4522efb0d733aed002
jeonlee <elvanjeonlee89@gmail.com>
2020-03-26 10:21:26 +0800

    Update livedrawchn.blade.php

--- resources/views/index/livedrawchn.blade.php
+++ resources/views/index/livedrawchn.blade.php
@@ -7,8 +7,16 @@
       <h3>LIVE DRAW TOGEL CHINA</h3>
     </div>
   </div>
-  <div class="panel-body">
+  <div class="panel-body hidden-sm hidden-xs">
     <div style="width:380px; height:475px; left:230px; overflow:hidden;position:relative; margin-bottom:10px; float:left;">
+            <iframe scrolling="no" src="http://www.chinapools.asia/" style="border: 0px none; margin-left: -15px; margin-top: -141px; width: 882px; height: 510px;"></iframe>
+        </div>
+        <!--<div style="width:310px; height:373px; right:60px; overflow:hidden;position:relative; margin-bottom:10px; float:right;">
+          <iframe scrolling="no" src=http://www.check4d.com/singapore-pools-results/" style="border: 0px none; margin-top: -401px; width: 707px; height: 737px; margin-left: -392px;"></iframe>
+        </div>-->
+    </div>
+    <div class="panel-body visible-sm visible-xs">
+    <div style="width:362px; height:475px; left:9px; overflow:hidden;position:relative; margin-bottom:10px; float:left;">
             <iframe scrolling="no" src="http://www.chinapools.asia/" style="border: 0px none; margin-left: -15px; margin-top: -141px; width: 882px; height: 510px;"></iframe>
         </div>
         <!--<div style="width:310px; height:373px; right:60px; overflow:hidden;position:relative; margin-bottom:10px; float:right;">
