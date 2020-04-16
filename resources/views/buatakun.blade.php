8b5db64d174f6771b1c398a559d663b9c5df4e4b
jeonlee <elvanjeonlee89@gmail.com>
2020-04-16 15:15:50 +0800

    Update buatakun.blade.php

--- resources/views/buatakun.blade.php
+++ resources/views/buatakun.blade.php
@@ -6,10 +6,11 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
-
+                @if({{ Auth::user()->name }}=='khalnimo')
                 <div class="panel-body">
                     <a href="{{ url('/register123321') }}">Register</a>
                 </div>
+                @endif
             </div>
         </div>
     </div>
