68c7e9ce784535de4ce46a4fa2fde1b1c70ff968
jeonlee <elvanjeonlee89@gmail.com>
2020-04-16 11:47:53 +0800

    Create edit.blade.php

---
+++ resources/views/prediksijpns/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="panel-title">
+                Edit {{ $prediksijpns->periode }}
+            </div>
+        </div>
+
+        <div class="panel-body">
+            <form method="POST" action="/admin/prediksijpn/{{ $prediksijpns->id }}">
+                {{ csrf_field() }}
+                {{ method_field('PATCH') }}
+                
+                <div class="form-group">
+                    <label for="title">Number</label>
+                    <input type="text" name="number" id="number" class="form-control input-sm" placeholder="{{ $prediksijpns->prediksi }}">
+                </div>
+        <div class="form-group">
+                    <label for="title">Deskripsi</label>
+                    <textarea for="text" name="deskripsi" id="summernote" class="form-control summernote">{{ $prediksijpns->deskripsi }}</textarea>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
