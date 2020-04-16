f8f3f13e0eb7a630f0141e643c3fa0413709b10e
jeonlee <elvanjeonlee89@gmail.com>
2020-04-16 10:57:52 +0800

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -241,7 +241,7 @@
                         <ul onmouseout="hover(this,'off')" onmouseover="hover(this,'on')" class="subMenu1">
                           <!-- <li><a style="padding:0 15px !important;" title="Lotto Results" href="http://jayatogel.com" target="_blank">www.jayatogel.com</a></li> -->
                           
-                          <li><a style="padding:0 15px !important;" title="Lotto Raffle Results" href="http://jayatgl2020.com" target="_blank">www.jayatgl2020.com</a></li>
+                          <li><a style="padding:0 15px !important;" title="Lotto Raffle Results" href="http://jayatogel20.com" target="_blank">www.jayatogel20.com</a></li>
                           <li><a style="padding:0 15px !important;" title="Lotto Raffle Results" href="http://103.10.200.43" target="_blank">103.10.200.43</a></li>
                         </ul>
                       </li>
