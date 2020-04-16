149533e4d5540498dbd21a22e14fc45070e51798
jeonlee <elvanjeonlee89@gmail.com>
2020-04-16 11:01:04 +0800

    Create Prediksijpn.php

---
+++ app/Prediksijpn.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prediksijpn extends Model
+{
+   protected $fillable = [
+        'tanggal', 'hari', 'prediksi', 'date','deskripsi'
+    ];
+    public function path()
+    {
+    	return '/categories/' . $this->id;
+    }
+
+    public function reports()
+    {
+    	return $this->hasMany('App\Report');
+    }
+
+    public function addReport(Report $report)
+    {
+    	return $this->reports()->save($report);
+    }
+}
