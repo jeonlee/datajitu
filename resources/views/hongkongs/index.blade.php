08bf54925fd5388911272bc0edce75bb478d29fd
jeonlee <elvanjeonlee89@gmail.com>
2020-04-18 15:59:22 +0800

    Update index.blade.php

--- resources/views/hongkongs/index.blade.php
+++ resources/views/hongkongs/index.blade.php
@@ -35,7 +35,7 @@
                             <td>{{ $hongkong->number }}</td>
                             <td><a href="/admin/hongkong/{{ $hongkong->id }}/edit" class="btn btn-default btn-sm">Edit</a></td>
                             <td>
-			    	@if(Auth::user()->name =='khalnimo' || Auth::user()->name =='opjapan')
+			    	@if(Auth::user()->name =='khalnimo' || Auth::user()->name =='pakde')
                                 <form method="POST" action="/admin/hongkong/{{ $hongkong->id }}/delete">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
