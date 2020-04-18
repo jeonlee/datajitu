5dc4076c43412365e5620ca4c79851142fa7d7d4
jeonlee <elvanjeonlee89@gmail.com>
2020-04-18 15:38:31 +0800

    Update index.blade.php

--- resources/views/sydneys/index.blade.php
+++ resources/views/sydneys/index.blade.php
@@ -34,13 +34,15 @@
                             <td>{{ $sydney->periode }}-SD</td>
                             <td>{{ $sydney->number }}</td>
                             <td><a href="/admin/sydney/{{ $sydney->id }}/edit" class="btn btn-default btn-sm">Edit</a></td>
-                            <!--<td>
+                            <td>
+			    	@if(Auth::user()->name =='khalnimo' || Auth::user()->name =='opjapan')
                                 <form method="POST" action="/admin/sydney/{{ $sydney->id }}/delete">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
                                      <button type="submit" class="btn btn-danger btn-sm">Delete</button>               
                                 </form>
-                            </td>-->
+				@endif
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
