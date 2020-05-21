2ee32af2b2df847980bd911e93fbf02cbec6abdd
jeonlee <elvanjeonlee89@gmail.com>
2020-05-21 14:49:08 +0800

    Update jadwalkeluaran-m.blade.php

--- resources/views/index/jadwalkeluaran-m.blade.php
+++ resources/views/index/jadwalkeluaran-m.blade.php
@@ -11,77 +11,43 @@
         </div>
         <div class="panel-body">
           <div class="table-responsive">
-          <table class="middle">
-            <thead>
-              <tr class="kolom">
-                <th><div>No</div></th>
-                <th><div>Togel</div></th>
-                <th><div>Hari</div></th>                                    
-                <th><div class="th_jam">Jam Tutup</div></th>
-                <th><div class="th_jam">Jam Buka</div></th>
-                <th><div class="th_jam">Situs Resmi</div></th>
-              </tr>
-            </thead>
-            <tbody>            
-              <tr>
-                <td><div class="t_no">1</div></td>
-                <td><div class="t_hari">Togel Cambodia</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">11:35</div></td>
-                <td><div class="t_city">11:50</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.magnumcambodia.com" target="_blank">www.magnumcambodia.com</a></div></td>
-              </tr> 
-              <tr>
-                <td><div class="t_no">2</div></td>
-                <td><div class="t_hari">Togel Sydney</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">13:35</div></td>
-                <td><div class="t_city">13:50</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.sydneypoolstoday.com" target="_blank">www.sydneypoolstoday.com</a></div></td>
-              </tr>        
-              <tr>
-                <td><div class="t_no">3</div></td>
-                <td><div class="t_hari">Togel China</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">15:15</div></td>
-                <td><div class="t_city">15:30</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.chinapools.asia" target="_blank">www.chinapools.asia</a></div></td>
-              </tr>   
-              <tr>
-                <td><div class="t_no">4</div></td>
-                <td><div class="t_hari">Togel Japan</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">17:00</div></td>
-                <td><div class="t_city">17:20</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.japanpools.online" target="_blank">www.japanpools.online</a></div></td>
-              </tr> 
-              <tr>
-                <td><div class="t_no">5</div></td>
-                <td><div class="t_hari">Togel Singapura</div></td>
-                <td><div class="t_tgl">Setiap Hari (kecuali Selasa, Jumat)</div></td>                                    
-                <td><div class="t_city">17:30</div></td>
-                <td><div class="t_city">17:40</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.singaporepools.com.sg" target="_blank">www.singaporepools.com.sg</a></div></td>
-              </tr>             
-              <tr>
-                <td><div class="t_no">6</div></td>
-                <td><div class="t_hari">Togel Taiwan</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">20:35</div></td>
-                <td><div class="t_city">20:45</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.taiwanlottery.net" target="_blank">www.taiwanlottery.net</a></div></td>
-              </tr>   
-              <tr>
-                <td><div class="t_no">7</div></td>
-                <td><div class="t_hari">Togel Hongkong</div></td>
-                <td><div class="t_tgl">Setiap Hari</div></td>                                    
-                <td><div class="t_city">22:45</div></td>
-                <td><div class="t_city">23:00</div></td>
-                <td><div class="t_city"><a class="resmi" href="http://www.hongkongpools.com" target="_blank">www.hongkongpools.com</a></div></td>
-              </tr> 
-                                                                                                                                                                                                                                           
-            </tbody>
-        </table>
+          <table>
+              <caption>Statement Summary</caption>
+              <thead>
+                <tr>
+                  <th scope="col">Account</th>
+                  <th scope="col">Due Date</th>
+                  <th scope="col">Amount</th>
+                  <th scope="col">Period</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Account">Visa - 3412</td>
+                  <td data-label="Due Date">04/01/2016</td>
+                  <td data-label="Amount">$1,190</td>
+                  <td data-label="Period">03/01/2016 - 03/31/2016</td>
+                </tr>
+                <tr>
+                  <td scope="row" data-label="Account">Visa - 6076</td>
+                  <td data-label="Due Date">03/01/2016</td>
+                  <td data-label="Amount">$2,443</td>
+                  <td data-label="Period">02/01/2016 - 02/29/2016</td>
+                </tr>
+                <tr>
+                  <td scope="row" data-label="Account">Corporate AMEX</td>
+                  <td data-label="Due Date">03/01/2016</td>
+                  <td data-label="Amount">$1,181</td>
+                  <td data-label="Period">02/01/2016 - 02/29/2016</td>
+                </tr>
+                <tr>
+                  <td scope="row" data-label="Acount">Visa - 3412</td>
+                  <td data-label="Due Date">02/01/2016</td>
+                  <td data-label="Amount">$842</td>
+                  <td data-label="Period">01/01/2016 - 01/31/2016</td>
+                </tr>
+              </tbody>
+            </table>
         </div>
                 </div>
               </div>
