550a3856a404a0bbdc5dbc1b6fea102a2824a61d
jeonlee <elvanjeonlee89@gmail.com>
2020-06-08 17:12:08 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,7 +40,7 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
-
+$app->forgetMiddleware('Illuminate\Http\FrameGuard');
 /*
 |--------------------------------------------------------------------------
 | Return The Application
