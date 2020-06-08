8ab3e513f49c206b7e53c8cfc99fe1410c56502e
jeonlee <elvanjeonlee89@gmail.com>
2020-06-08 15:39:30 +0800

    Update analisiscambodia.blade.php

--- resources/views/index/analisiscambodia.blade.php
+++ resources/views/index/analisiscambodia.blade.php
@@ -597,7 +597,7 @@
                     $dasarbk3 = substr("$dasarbk9",0,1);
                     $dasarbk4 = substr("$dasarbk9",1,1);
 		   if($dasarbk4==''){
-		   $dasarbk4=0;
+		    $dasarbk4=0;
 		   }
                     $dasarbk5 = $dasarbk3 + $dasarbk4;
                  ?>
@@ -646,6 +646,9 @@
                     $dasargg = $tiga + $empat;
                     $dasargg3 = substr("$dasargg",0,1);
                     $dasargg4 = substr("$dasargg",1,1);
+                    if($dasargg4==''){
+                      $dasargg4=0;
+                    }
                     $dasargg5 = $dasargg3 + $dasargg4;
                     $dasargg1 = $dasargg5 % 2;
                     $dg = 0;
